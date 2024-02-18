9e6d410c8726b13b25dca0c7de7cc0160ecb38e6
Nico_le <156796855+NicoleMaini@users.noreply.github.com>
2024-02-18 21:39:50 +0100

    svg animate

--- js/script.js
+++ js/script.js
@@ -15,3 +15,41 @@ window.addEventListener("scroll", function () {
     btn.classList.remove("btn-green");
   }
 });
+
+// ANIMATE SVG
+
+const svgM = document.querySelectorAll("header svg g path");
+console.log(svgM);
+
+const svgMReturn = [];
+// console.log(svgMReturn);
+
+const svgAnimateReturn = () => {
+  const blockLoop = setInterval(() => {
+    const randNum = Math.floor(Math.random() * svgMReturn.length);
+    svgMReturn[randNum].classList.remove("svg-animate");
+    svgMReturn[randNum].classList.add("svg-animate-return");
+    svgMReturn.pop(svgMReturn[randNum]);
+    if (svgMReturn.length === 0) {
+      clearInterval(blockLoop);
+    }
+  }, 100);
+  blockLoop;
+};
+
+const svgAnimate = setInterval(() => {
+  if (svgMReturn.length === 150) {
+    clearInterval(svgAnimate);
+    svgAnimateReturn(svgMReturn);
+  } else {
+    const randNum = Math.floor(Math.random() * svgM.length);
+    svgM[randNum].classList.add("svg-animate");
+    svgMReturn.push(svgM[randNum]);
+  }
+}, 100);
+
+setInterval(() => {
+  if (svgMReturn.length === 0) {
+    svgAnimate;
+  }
+}, 1);
