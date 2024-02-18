d912544a1dceb051f05a89de0eb2a144ff6b93de
Nico_le <156796855+NicoleMaini@users.noreply.github.com>
2024-02-18 12:18:30 +0100

    navbar complete

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+// SCROLL FUNCTION
+
+const point = document.querySelector(".down-part-header-title");
+console.log(point);
+
+const nav = document.querySelector(".fix-header");
+const btn = document.querySelector(".btn");
+
 window.addEventListener("scroll", function () {
-  document.querySelector(".fix-header").classList.add("bg-color-scroll");
+  if (window.scrollY > point.scrollHeight) {
+    nav.classList.add("bg-color-scroll");
+    btn.classList.add("btn-green");
+  } else {
+    nav.classList.remove("bg-color-scroll");
+    btn.classList.remove("btn-green");
+  }
 });
